f75e06c9ad669de943b1f022f563d67252d40dbd
lijicheng <you@example.com>
2018-12-21 12:03:19 +0800

    112233222222111111

--- m.invitation/js/main.js
+++ m.invitation/js/main.js
@@ -1,3 +1,36 @@
+//禁止下拉
+var overscroll = function (el) {
+	el.addEventListener('touchstart', function () {
+		var top = el.scrollTop
+			, totalScroll = el.scrollHeight
+			, currentScroll = top + el.offsetHeight
+		//If we're at the top or the bottom of the containers
+		//scroll, push up or down one pixel.
+		//
+		//this prevents the scroll from "passing through" to
+		//the body.
+		if (top === 0) {
+			el.scrollTop = 1
+		} else if (currentScroll === totalScroll) {
+			el.scrollTop = top - 1
+		}
+	})
+	el.addEventListener('touchmove', function (evt) {
+		//if the content is actually scrollable, i.e. the content is long enough
+		//that scrolling can occur
+		if (el.offsetHeight < el.scrollHeight)
+			evt._isScroller = true
+	})
+}
+overscroll(document.querySelector('.page'));
+document.body.addEventListener('touchmove', function (evt) {
+	//In this case, the default behavior is scrolling the body, which
+	//would result in an overflow.  Since we don't want that, we preventDefault.
+	if (!evt._isScroller) {
+		evt.preventDefault()
+	}
+})
+
 var imgurl = [];
 var bgimgurl = ['images/page_bg_02.png', 'images/page_bg_03.png', 'images/page_bg_04.png']
 $("img").each(function (i, t) {
@@ -44,6 +77,9 @@ setInterval(function () {
 
 
 $(".nextShow").on('click', function () {
+	if ($(this).parents().hasClass("page8")) {
+		return
+	}
 	let page = $(this).attr("data-page");
 	if (page) {
 		debugger
